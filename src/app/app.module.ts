b219e684bec7266aef997ddae1ea9a256d3b53e6
dmsb <diogomsb1@gmail.com>
2018-09-09 23:15:43 -0300

    Criada configuração de comunicação com o servidor e solicitação de access token com CORS.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+import { FooterModule } from './footer/footer.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginModule } from './login/login.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    HeaderModule,
+    FooterModule,
+    LoginModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
